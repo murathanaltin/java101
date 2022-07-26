d23579c0dd373eb77dca3792c5d18e480d566f70
Murat <mrthnaltn@gmail.com>
2022-07-26 15:26:31 +0300

    First commit.

--- src/ust2.java
+++ src/ust2.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class ust2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int i = 1, number;
+        int number;
 
         System.out.print("Number: ");
         number = input.nextInt();
 
-        for (i = 1; i <= number; i *= 4) {
+        for (int i = 1; i <= number; i *= 4) {
             System.out.println(i);
         }
 
-        for (i = 1; i <= number; i *= 5) {
+        for (int i = 1; i <= number; i *= 5) {
             System.out.println(i);
         }
     }
